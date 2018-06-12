38fd365d04b08b06f9b7f55808191974629df9b9
maslov <dev.maslov.a@gmail.com>
2018-06-12 11:42:51 +0300

    finished footer

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-accordeon/toggler-accordeon.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-accordeon/toggler-accordeon.ts
@@ -4,8 +4,24 @@ import $ from 'jquery/dist/jquery';
 export default class TogglerAccordeon extends Component {
 
     readyCallback(): void {
-        console.log($);
-        $('.footer').wrap('<div class="test"></div>');
+        const triggerSelector = $(this).attr('trigger');
+        const triggers = $(triggerSelector);
+        const classToToggle = $(this).attr('class-to-toggle');
+
+        triggers.each(function(){
+            const _self = $(this);
+            const targetSelector = _self.data('toggle-target');
+            const target = $(targetSelector);
+
+            _self.on('click', function(){
+                target.toggleClass(classToToggle);
+                if (!target.hasClass(classToToggle)) {
+                    _self.addClass('active');
+                    return;
+                }
+                _self.removeClass('active');
+            });
+        });
     }
 
 }
