1ee91ad3c684e4e2021253b66cd8e3d401626a3a
Alexandr Veselov <alexander.veselov@spryker.com>
2018-09-11 11:38:49 +0000

    SUITE-624: add preCheckQuantityPlugin, created readonly

---
+++ src/Pyz/Zed/ProductBundle/Communication/Plugin/Cart/QuantityPreCheckPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductBundle\Communication\Plugin\Cart;
+
+use Generated\Shared\Transfer\CartChangeTransfer;
+use Spryker\Zed\Cart\Dependency\CartPreCheckPluginInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \Spryker\Zed\ProductBundle\Business\ProductBundleFacadeInterface getFacade()
+ * @method \Spryker\Zed\ProductBundle\Communication\ProductBundleCommunicationFactory getFactory()
+ */
+class CartBundleAvailabilityPreCheckPlugin extends AbstractPlugin implements CartPreCheckPluginInterface
+{
+    /**
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\CartChangeTransfer $cartChangeTransfer
+     *
+     * @return \Generated\Shared\Transfer\CartPreCheckResponseTransfer
+     */
+    public function check(CartChangeTransfer $cartChangeTransfer)
+    {
+         return $this->getFacade()->preCheckCartAvailability($cartChangeTransfer);
+    }
+}
