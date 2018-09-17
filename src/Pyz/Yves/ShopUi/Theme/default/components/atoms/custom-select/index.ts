d95cbf41b1ab049203629714c0eaf55c49b9327e
lysenko <nikita.lysenko@spryker.com>
2018-09-17 19:26:51 +0300

    ts import from vendor fix

--- src/Pyz/Yves/ShopUi/Theme/default/components/atoms/custom-select/index.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/atoms/custom-select/index.ts
@@ -1,5 +1,4 @@
-import 'select2/src/scss/core';
-
+import 'select2/src/scss/core.scss';
 import './custom-select.scss';
 import register from 'ShopUi/app/registry';
 export default register('custom-select', () => import(/* webpackMode: "eager" */'./custom-select'));
