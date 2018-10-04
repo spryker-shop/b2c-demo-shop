828960fecbf9be6c923cc3d906eaab1fe25dde82
Alexander Veselov <alexander.veselov@spryker.com>
2018-10-04 12:40:11 +0000

    SUITE-725: added dashboard, charts

---
+++ src/Pyz/Zed/Chart/ChartDependencyProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Chart;
+
+use Spryker\Zed\Chart\ChartDependencyProvider as SprykerChartDependencyProvider;
+use Spryker\Zed\SalesStatistics\Communication\Plugin\CountOrderChartPlugin;
+use Spryker\Zed\SalesStatistics\Communication\Plugin\StatusOrderChartPlugin;
+use Spryker\Zed\SalesStatistics\Communication\Plugin\TopOrdersChartPlugin;
+
+class ChartDependencyProvider extends SprykerChartDependencyProvider
+{
+    /**
+     * @return \Spryker\Shared\Chart\Dependency\Plugin\ChartPluginInterface[]
+     */
+    protected function getChartPlugins(): array
+    {
+        return [
+            new CountOrderChartPlugin(),
+            new StatusOrderChartPlugin(),
+            new TopOrdersChartPlugin(),
+        ];
+    }
+}
