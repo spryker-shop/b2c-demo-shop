dc7a299cc30b902c6e577f92cd20ed6020aae4c3
Alexander Veselov <alexander.veselov@spryker.com>
2018-10-05 13:04:12 +0000

    SUITE-733: added alternative and discontinued products

--- src/Orm/Zed/ProductDiscontinued/Persistence/SpyProductDiscontinuedNoteQuery.php
+++ src/Orm/Zed/ProductDiscontinued/Persistence/SpyProductDiscontinuedNoteQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\ProductDiscontinued\Persistence;
 
 use Orm\Zed\ProductDiscontinued\Persistence\Base\SpyProductDiscontinuedNoteQuery as BaseSpyProductDiscontinuedNoteQuery;
@@ -10,11 +15,10 @@ use Orm\Zed\ProductDiscontinued\Persistence\Base\SpyProductDiscontinuedNoteQuery
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyProductDiscontinuedNoteQuery extends BaseSpyProductDiscontinuedNoteQuery
 {
-
 }
