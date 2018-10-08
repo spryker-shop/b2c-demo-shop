16d969c27e0038639415f3ebcd326ba8cdca66a5
Alexander Veselov <alexander.veselov@spryker.com>
2018-10-08 07:18:12 +0000

    SUITE-731: fixed deleting bundle from cart

---
+++ src/Pyz/Zed/PersistentCart/PersistentCartDependencyProvider.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\PersistentCart;
+
+use Spryker\Zed\PersistentCart\PersistentCartDependencyProvider as SprykerPersistentCartDependencyProvider;
+use Spryker\Zed\PersistentCartExtension\Dependency\Plugin\QuoteItemFinderPluginInterface;
+use Spryker\Zed\ProductBundle\Communication\Plugin\PersistentCart\BundleProductQuoteItemFinderPlugin;
+use Spryker\Zed\ProductBundle\Communication\Plugin\PersistentCart\RemoveBundleChangeRequestExpanderPlugin;
+
+class PersistentCartDependencyProvider extends SprykerPersistentCartDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\PersistentCartExtension\Dependency\Plugin\QuoteItemFinderPluginInterface
+     */
+    protected function getQuoteItemFinderPlugin(): QuoteItemFinderPluginInterface
+    {
+        return new BundleProductQuoteItemFinderPlugin();
+    }
+
+    /**
+     * @return \Spryker\Zed\PersistentCartExtension\Dependency\Plugin\CartChangeRequestExpandPluginInterface[]
+     */
+    protected function getRemoveItemsRequestExpanderPlugins(): array
+    {
+        return [
+            new RemoveBundleChangeRequestExpanderPlugin(),
+        ];
+    }
+}
