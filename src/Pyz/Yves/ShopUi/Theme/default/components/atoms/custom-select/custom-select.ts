c0ad58107c855815724a62b6da40f023190baae6
sergeirybalko <sergeirybalko@gmail.com>
2018-10-09 17:52:13 +0300

    SUITE-738: Complete refactor custom select

--- src/Pyz/Yves/ShopUi/Theme/default/components/atoms/custom-select/custom-select.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/atoms/custom-select/custom-select.ts
@@ -6,14 +6,14 @@ export default class CustomSelect extends Component {
 
     protected readyCallback(): void {
         const select2 = select;
-        const targetSelect = $(this).find(`.js-${this.name}`);
-        if($('body').hasClass('no-touch')) {
+        const targetSelect = this.querySelector(`.js-${this.name}`);
+        if(document.body.classList.contains('no-touch')) {
             $(targetSelect).select2({
                 minimumResultsForSearch: Infinity
             });
             $(targetSelect).parents('body').on('click', function (e) {
-                const eventTarget = $(e.target);
-                if (eventTarget.hasClass('select2-container--open')) {
+                const eventTarget = e.target;
+                if (eventTarget.classList.contains('select2-container--open')) {
                     $(targetSelect).select2('close');
                 }
             });
