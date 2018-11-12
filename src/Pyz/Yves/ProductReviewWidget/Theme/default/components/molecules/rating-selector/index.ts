8f806d5e7f850b623e03212d67fff5d3bd9a3737
danil.moroz <danil.moroz@spryker.com>
2018-11-12 16:06:01 +0200

    SUITE-793: fix after review

--- src/Pyz/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/index.ts
+++ src/Pyz/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/index.ts
@@ -1,3 +1,3 @@
 import './rating-selector.scss';
 import register from 'ShopUi/app/registry';
-export default register('rating-selector', () => import(/* webpackMode: "lazy" */'./rating-selector'));
+export default register('rating-selector', () => import(/* webpackMode: "lazy" */'SprykerShop/product-review-widget/src/SprykerShop/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/rating-selector'));
