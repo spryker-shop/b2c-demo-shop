4337766cb2d833a4cd517342a22f653e69f0f346
Alexander Veselov <alexander.veselov@spryker.com>
2018-11-28 11:14:34 +0000

    SUITE-870: add category image in category dataimport

---
+++ src/Pyz/Zed/CategoryDataImport/Business/Model/CategoryWriterStep.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Zed\CategoryDataImport\Business\Model;
+
+use Spryker\Zed\CategoryDataImport\Business\Model\CategoryWriterStep as SprykerCategoryWriterStep;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class CategoryWriterStep extends SprykerCategoryWriterStep
+{
+    const KEY_CATEGORY_IMAGE_NAME = 'category_image_name';
+}
