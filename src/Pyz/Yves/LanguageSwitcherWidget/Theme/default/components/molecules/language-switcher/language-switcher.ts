257f8aaa5e9ad2f3e99076c3b15a68d3dc989177
maslov <dev.maslov.a@gmail.com>
2018-12-17 16:18:51 +0200

    SUITE-944: fixed according with PR

--- src/Pyz/Yves/LanguageSwitcherWidget/Theme/default/components/molecules/language-switcher/language-switcher.ts
+++ src/Pyz/Yves/LanguageSwitcherWidget/Theme/default/components/molecules/language-switcher/language-switcher.ts
@@ -3,17 +3,13 @@ import Component from 'ShopUi/models/component';
 export default class LanguageSwitcher extends Component {
     readonly selectList: HTMLSelectElement[];
 
-    constructor() {
-        super();
-        this.selectList = <HTMLSelectElement[]>Array.from(document.querySelectorAll(`.${this.jsName}`));
-    }
-
     protected readyCallback(): void {
         this.mapEvents();
     }
 
     protected mapEvents(): void {
-        this.selectList.forEach((select: HTMLSelectElement) => select.addEventListener('change', (event: Event) => this.onTriggerChange(event)));
+        const select = this.querySelector(`.${this.jsName}`);
+        select.addEventListener('change', (event: Event) => this.onTriggerChange(event));
     }
 
     protected onTriggerChange(event: Event): void {
