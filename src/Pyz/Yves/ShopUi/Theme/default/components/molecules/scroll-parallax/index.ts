06f9896c628198d4e3f6db3d0a07469fb615a624
maslov <dev.maslov.a@gmail.com>
2018-12-21 11:31:37 +0200

    SUITE-985: Created paralax

---
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/scroll-parallax/index.ts
@@ -0,0 +1,3 @@
+import './scroll-parallax.scss';
+import register from 'ShopUi/app/registry';
+export default register('scroll-parallax', () => import(/* webpackMode: "lazy" */'./scroll-parallax'));
---
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/scroll-parallax/scroll-parallax.ts
@@ -0,0 +1,115 @@
+import Component from 'ShopUi/models/component';
+import $ from 'jquery/dist/jquery';
+
+const DIRECTIONS = {
+    TOP: 'top',
+    DOWN: 'down'
+}
+
+export default class ScrollParallax extends Component {
+    protected target: HTMLElement;
+    protected wrapper: HTMLElement;
+    windowHeight: number;
+    windowWidth: number;
+    wrapperHeight: number;
+    distanceToWrapper: number;
+    initialized: boolean
+    
+    readyCallback(): void {
+        this.wrapper = <HTMLElement>document.querySelector(this.wrapperSelector);
+        this.target = <HTMLElement>this.wrapper.querySelector(this.targetSelector);
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
+        this.wrapperHeight = this.wrapper.offsetHeight;
+        this.distanceToWrapper = this.getDistanceToWrapper();
+        this.initialized = false;
+
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+        window.addEventListener('resize', () => setTimeout(() => this.recalculateDimensions(), 300));
+        window.addEventListener('scroll', this.checkBreakpointsToScroll.bind(this));
+    }
+
+    protected recalculateDimensions(): void {
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
+        this.wrapperHeight = this.wrapper.offsetHeight;
+        this.distanceToWrapper = this.getDistanceToWrapper();
+    }
+
+    protected checkBreakpointsToScroll(): void {
+        if(this.minBreakPoint !== NaN && this.maxBreakPoint !== NaN) {
+            if(this.minBreakPoint < this.windowWidth && this.maxBreakPoint > this.windowWidth) {
+                this.moveTarget();
+                return;
+            }
+            this.cleanOffset();
+        }
+    }
+
+    protected cleanOffset(): void {
+        if(this.initialized === true) {
+            this.initialized = false;
+            this.target.removeAttribute('style');
+        }
+    }
+
+    protected moveTarget(): void {
+        let scrollToBottomScreen: number = window.scrollY + this.windowHeight;
+        let targetOffset: string = '';
+
+        if(scrollToBottomScreen > this.distanceToWrapper) {
+            if (this.motionDirection === DIRECTIONS.TOP) {
+                targetOffset = `-${this.getTargetOffest(scrollToBottomScreen)}`;
+            }
+            if (this.motionDirection === DIRECTIONS.DOWN) {
+                targetOffset = this.getTargetOffest(scrollToBottomScreen);
+            }
+            if (targetOffset !== '') {
+                this.target.style.transform = `translateY(${targetOffset})`;
+                this.initialized = true;
+            }
+        }
+    }
+
+    protected getTargetOffest(scrollToBottomScreen) {
+        return (scrollToBottomScreen - this.distanceToWrapper) / this.motionRatio + 'px';
+    }
+
+    protected getDistanceToWrapper(): number {
+        let element: HTMLElement = this.wrapper;
+        let yPosition: number  = 0;
+
+        while(element) {
+            yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+            element = <HTMLElement>element.offsetParent;
+        }
+        return yPosition;
+    }
+
+    get targetSelector(): string {
+        return this.getAttribute('target-selector');
+    }
+
+    get wrapperSelector(): string {
+        return this.getAttribute('wrapper-selector');
+    }
+
+    get motionRatio(): number {
+        return +this.getAttribute('motion-ratio');
+    }
+
+    get motionDirection(): string {
+        return this.getAttribute('motion-direction');
+    }
+
+    get minBreakPoint(): number {
+        return +this.getAttribute('breakpoint-min');
+    }
+
+    get maxBreakPoint(): number {
+        return +this.getAttribute('breakpoint-max');
+    }
+}
