6d3f062def4cb16cb45082ee308ae78b6995bad4
Sergey Palkin <sergey.palkin@spryker.com>
2019-05-31 17:45:13 +0300

    CC-4938: Email template error fix (draft).

---
+++ src/Pyz/Yves/Twig/TwigDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\Twig;
+
+use Spryker\Yves\Twig\TwigDependencyProvider as SprykerTwigDependencyProvider;
+use SprykerShop\Yves\MoneyWidget\Plugin\Twig\MoneyTwigPlugin;
+
+class TwigDependencyProvider extends SprykerTwigDependencyProvider
+{
+    /**
+     * @return \Spryker\Shared\TwigExtension\Dependency\Plugin\TwigPluginInterface[]
+     */
+    protected function getTwigPlugins(): array
+    {
+        return [
+            new MoneyTwigPlugin(),
+        ];
+    }
+}
---
+++ src/Pyz/Zed/Twig/TwigDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Twig;
+
+use Spryker\Zed\Money\Communication\Plugin\Twig\MoneyTwigPlugin;
+use Spryker\Zed\Twig\TwigDependencyProvider as SprykerTwigDependencyProvider;
+
+class TwigDependencyProvider extends SprykerTwigDependencyProvider
+{
+    /**
+     * @return \Spryker\Shared\TwigExtension\Dependency\Plugin\TwigPluginInterface[]
+     */
+    protected function getTwigPlugins(): array
+    {
+        return [
+            new MoneyTwigPlugin(),
+        ];
+    }
+}
