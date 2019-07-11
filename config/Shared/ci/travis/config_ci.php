9c7b1f8b50465838476dfad4ee973b253fe7a35d
denys.sokolov <denys.sokolov@spryker.com>
2019-07-11 14:46:11 +0300

    CC-5442: enable event behavior triggering active.

--- config/Shared/ci/travis/config_ci.php
+++ config/Shared/ci/travis/config_ci.php
@@ -158,7 +158,7 @@ $config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
 $config[LogConstants::LOG_LEVEL] = Logger::CRITICAL;
 
 // ---------- EventBehavior
-$config[EventBehaviorConstants::EVENT_BEHAVIOR_TRIGGERING_ACTIVE] = false;
+$config[EventBehaviorConstants::EVENT_BEHAVIOR_TRIGGERING_ACTIVE] = getenv('TEST_GROUP') === 'acceptance';
 
 // ---------- Trusted hosts
 $config[ApplicationConstants::YVES_TRUSTED_HOSTS] = [
