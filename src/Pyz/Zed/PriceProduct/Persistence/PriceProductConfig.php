f76372b6f0742fca0662fcc39e81db35cd75edbe
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-10-02 14:43:07 +0300

    PS-10931 PS-9909 added const to price product config

--- src/Pyz/Zed/PriceProduct/Persistence/PriceProductConfig.php
+++ src/Pyz/Zed/PriceProduct/Persistence/PriceProductConfig.php
@@ -1,14 +1,17 @@
 <?php
 
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
  */
+namespace Pyz\Zed\PriceProduct;
 
-namespace Pyz\Zed\PriceProduct\Persistence;
+use Spryker\Zed\PriceProduct\PriceProductConfig as SprykerPriceProductConfig;
 
-
-class PriceProductConfig
+class PriceProductConfig extends SprykerPriceProductConfig
 {
-
+    /**
+     * Decides if orphan store prices need to be cleared after every product price update.
+     */
+    protected const IS_DELETE_ORPHAN_STORE_PRICES_ON_SAVE_ENABLED = false;
 }
