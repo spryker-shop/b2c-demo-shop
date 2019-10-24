08c3f4e2dd7c7a2ee78b10ec60c3033f081a5c8e
Denis Gnusov <denis.gnusov@spryker.com>
2019-10-24 14:06:02 +0300

    GLUE-6850 Fixed spacings in new files

--- src/Pyz/Glue/CustomersRestApi/CustomerAccessRestApiConfig.php
+++ src/Pyz/Glue/CustomersRestApi/CustomerAccessRestApiConfig.php
@@ -1,15 +1,19 @@
 <?php
+
 /**
- * This file is part of the Spryker Suite.
+ * This file is part of the Spryker Commerce OS.
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
+
 namespace Pyz\Glue\CustomerAccessRestApi;
+
 use Pyz\Glue\CheckoutRestApi\CheckoutRestApiConfig;
 use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
 use Spryker\Glue\CustomerAccessRestApi\CustomerAccessRestApiConfig as SprykerCustomerAccessRestApiConfig;
 use Spryker\Glue\ProductPricesRestApi\ProductPricesRestApiConfig;
 use Spryker\Glue\WishlistsRestApi\WishlistsRestApiConfig;
 use Spryker\Shared\CustomerAccess\CustomerAccessConfig;
+
 class CustomerAccessRestApiConfig extends SprykerCustomerAccessRestApiConfig
 {
     protected const CUSTOMER_ACCESS_CONTENT_TYPE_TO_RESOURCE_TYPE_MAPPING = [
