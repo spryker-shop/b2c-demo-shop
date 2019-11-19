2c0270251f9ca016135adf3f8a0e5340a9541321
Daniel Kleba <danylo.kleba@spryker.com>
2019-11-19 08:57:53 +0000

    CC-6705: Integrate delivery method per store into B2C demoshop.
    * Update `composer.lock` dependencies.
    * Modify `Zed\Console\ConsoleDependencyProvider`.
    * Add `Zed\ShipmentDataImport\ShipmentDataImportConfig`.
    * Add `Zed\ShipmentGui\ShipmentGuiDependencyProvider`.

---
+++ src/Pyz/Zed/ShipmentGui/ShipmentGuiDependencyProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pyz\Zed\ShipmentGui;
+
+use Spryker\Zed\Kernel\Communication\Form\FormTypeInterface;
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\Money\Communication\Plugin\Form\MoneyCollectionFormTypePlugin;
+use Spryker\Zed\ShipmentGui\ShipmentGuiDependencyProvider as SprykerShipmentGuiDependencyProvider;
+use Spryker\Zed\Store\Communication\Plugin\Form\StoreRelationToggleFormTypePlugin;
+
+class ShipmentGuiDependencyProvider extends SprykerShipmentGuiDependencyProvider
+{
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Communication\Form\FormTypeInterface
+     */
+    protected function getMoneyCollectionFormTypePlugin(Container $container): FormTypeInterface
+    {
+        return new MoneyCollectionFormTypePlugin();
+    }
+
+    /**
+     * @return \Spryker\Zed\Kernel\Communication\Form\FormTypeInterface
+     */
+    protected function getStoreRelationFormTypePlugin(): FormTypeInterface
+    {
+        return new StoreRelationToggleFormTypePlugin();
+    }
+}
