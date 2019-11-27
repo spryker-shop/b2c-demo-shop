5935dc1bc5bec2a4520469989dc79414a7235a5d
Vasily Rodin <vasily.rodin@spryker.com>
2019-11-27 15:13:00 +0000

    TE-4304 Update composer

--- src/Pyz/Zed/Router/RouterConfig.php
+++ src/Pyz/Zed/Router/RouterConfig.php
@@ -7,35 +7,19 @@
 
 namespace Pyz\Zed\Router;
 
-use Spryker\Shared\Kernel\KernelConstants;
 use Spryker\Zed\Router\RouterConfig as SprykerRouterConfig;
-use Zend\Filter\FilterChain;
-use Zend\Filter\StringToLower;
-use Zend\Filter\Word\CamelCaseToDash;
 
 class RouterConfig extends SprykerRouterConfig
 {
     /**
-     * @project Only required for nonsplit-repositories, do not use this in project.
-     *
      * @return array
      */
     public function getControllerDirectories(): array
     {
-        $controllerDirectories = [];
+        $controllerDirectories = parent::getControllerDirectories();
 
-        foreach ($this->get(KernelConstants::PROJECT_NAMESPACES) as $projectNamespace) {
-            $controllerDirectories[] = sprintf('%s/%s/Zed/*/Communication/Controller/', APPLICATION_SOURCE_DIR, $projectNamespace);
-        }
-
-        $filterChain = new FilterChain();
-        $filterChain
-            ->attach(new CamelCaseToDash())
-            ->attach(new StringToLower());
-
-        foreach ($this->get(KernelConstants::CORE_NAMESPACES) as $coreNamespace) {
-            $controllerDirectories[] = sprintf('%s/%s/*/src/%s/Zed/*/Communication/Controller/', APPLICATION_VENDOR_DIR, $filterChain->filter($coreNamespace), $coreNamespace);
-        }
+        $controllerDirectories[] = sprintf('%s/spryker-sdk/*/src/*/Zed/*/Communication/Controller/', APPLICATION_VENDOR_DIR);
+        $controllerDirectories[] = sprintf('%s/spryker-eco/*/src/*/Zed/*/Communication/Controller/', APPLICATION_VENDOR_DIR);
 
         return array_filter($controllerDirectories, 'glob');
     }
