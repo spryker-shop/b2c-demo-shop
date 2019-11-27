a485f9ee25d9f6f5ebef84d773aa6c9f19a660fe
ilyakubanov <ilya.kubanov@spryker.com>
2019-11-27 17:39:14 +0200

    CMS-2749: fixes

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -6,7 +6,7 @@
  */
 
 define('APPLICATION_ROOT_DIR', __DIR__);
-define('APPLICATION_VENDOR_DIR', APPLICATION_ROOT_DIR . '/vendor/spryker');
+define('APPLICATION_VENDOR_DIR', APPLICATION_ROOT_DIR . '/vendor');
 define('APPLICATION_SOURCE_DIR', APPLICATION_ROOT_DIR . '/src');
 define('APPLICATION', '');
 define('APPLICATION_ENV', '');
