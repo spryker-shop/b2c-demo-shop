b05fe2e7579cadeeb081bca4a8b29ff19d8541de
denys.sokolov <denys.sokolov@spryker.com>
2019-12-06 19:36:58 +0200

    CC-7507: code style fixes.

--- src/Pyz/Zed/PriceProduct/Persistence/PriceProductConfig.php
+++ src/Pyz/Zed/PriceProduct/Persistence/PriceProductConfig.php
@@ -4,6 +4,7 @@
  * This file is part of the Spryker Commerce OS.
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
+
 namespace Pyz\Zed\PriceProduct;
 
 use Spryker\Zed\PriceProduct\PriceProductConfig as SprykerPriceProductConfig;
