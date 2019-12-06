3f02e1e2d3941b7b9374945d7593c55bd3e1d66b
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-12-06 14:36:35 +0200

    remove deprecations from environment configs

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -103,9 +103,6 @@ $config[UserConstants::USER_SYSTEM_USERS] = [
     'yves_system',
 ];
 // For a better performance you can turn off Zed authentication
-$AUTH_ZED_ENABLED = false;
-$config[AuthConstants::AUTH_ZED_ENABLED] = $AUTH_ZED_ENABLED;
-$config[ZedRequestConstants::AUTH_ZED_ENABLED] = $AUTH_ZED_ENABLED;
 $config[AuthConstants::AUTH_DEFAULT_CREDENTIALS] = [
     'yves_system' => [
         'rules' => [
