feb99eb78a7130ad3f9afaad363f92ad119f6612
Anton Zubariev <anton.zubariev@spryker.com>
2020-07-20 12:18:49 +0300

    [PS-13852] Add to Cart from the Catalog page integration

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -31,6 +31,7 @@ use SprykerShop\Yves\CartNoteWidget\Widget\CartNoteFormWidget;
 use SprykerShop\Yves\CartPage\Widget\AddItemsFormWidget;
 use SprykerShop\Yves\CartPage\Widget\AddToCartFormWidget;
 use SprykerShop\Yves\CartPage\Widget\CartChangeQuantityFormWidget;
+use SprykerShop\Yves\CartPage\Widget\ProductAbstractAddToCartButtonWidget;
 use SprykerShop\Yves\CartPage\Widget\RemoveFromCartFormWidget;
 use SprykerShop\Yves\CategoryImageStorageWidget\Widget\CategoryImageStorageWidget;
 use SprykerShop\Yves\CheckoutWidget\Widget\CheckoutBreadcrumbWidget;
@@ -158,6 +159,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             AddItemsFormWidget::class,
             CartChangeQuantityFormWidget::class,
             RemoveFromCartFormWidget::class,
+            ProductAbstractAddToCartButtonWidget::class,
         ];
     }
 
