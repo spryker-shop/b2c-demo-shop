93abb1013ac01ba29988a83ed2df420803529600
ilyakubanov <ilya.kubanov@spryker.com>
2020-08-10 14:17:29 +0300

    CC-10442: Integration of GLUE-10680 in the B2C

--- src/Pyz/Zed/Checkout/CheckoutDependencyProvider.php
+++ src/Pyz/Zed/Checkout/CheckoutDependencyProvider.php
@@ -17,6 +17,7 @@ use Spryker\Zed\Discount\Communication\Plugin\Checkout\VoucherDiscountMaxUsageCh
 use Spryker\Zed\GiftCard\Communication\Plugin\GiftCardOrderItemSaverPlugin;
 use Spryker\Zed\GiftCardMailConnector\Communication\Plugin\Checkout\SendEmailToGiftCardUser;
 use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentMethodValidityCheckoutPreConditionPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentOrderSaverPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentPostCheckPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentPreCheckPlugin;
@@ -50,6 +51,7 @@ class CheckoutDependencyProvider extends SprykerCheckoutDependencyProvider
             new SalesOrderThresholdCheckoutPreConditionPlugin(), #SalesOrderThresholdFeature
             new VoucherDiscountMaxUsageCheckoutPreConditionPlugin(),
             new ShipmentCheckoutPreCheckPlugin(),
+            new PaymentMethodValidityCheckoutPreConditionPlugin(),
         ];
     }
 
