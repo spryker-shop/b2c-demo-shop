26871c6dc220dd8d41d67badeb4053434ec18610
zamlelyi <nikolay.zamlely@spryker.com>
2020-08-27 13:51:40 +0300

    CC-10822: Remove unnecessary registration of non-existent product-item-list web component

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/product-item-list/index.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/product-item-list/index.ts
@@ -1,3 +1,1 @@
 import './product-item-list.scss';
-import register from 'ShopUi/app/registry';
-export default register('product-item-list', () => import(/* webpackMode: "lazy" */'../product-item/product-item'));
