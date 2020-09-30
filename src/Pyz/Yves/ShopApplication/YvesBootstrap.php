843013efbab7cdc4f06891fb6bddbc0ce10627da
Pavel Asaulenko <asaulenko.pavel@gmail.com>
2020-09-30 17:18:51 +0300

    Integration: TE-7460, 7566, 7336, 7367 (#831)
    * TE-7460 Make Silex optional integration
    
    * TE-7460 Added codeception/lib-innerbrowser to dependencies
    
    * TE-4571 Added CartTwigPlugin
    
    * TE-4571 Updated composer files
    
    * TE-4571 Updated composer files
    
    * TE-4571 Removed CartServiceProvider
    
    * TE-4571 CR edits
    
    * TE-4571 Removed redundant class constant
    
    * TE-4571 Updated composer lock file
    
    * TE-4571 Updated composer lock file
    
    * TE-7562 Fixed typo in cart-block.twig
    
    * TE-7562 Fixed wrong usages of cart quantity in several templates

--- src/Pyz/Yves/ShopApplication/YvesBootstrap.php
+++ src/Pyz/Yves/ShopApplication/YvesBootstrap.php
@@ -7,18 +7,8 @@
 
 namespace Pyz\Yves\ShopApplication;
 
-use Pyz\Yves\CartPage\Plugin\Provider\CartServiceProvider;
-use Spryker\Yves\Application\Plugin\ServiceProvider\KernelLogServiceProvider;
-use SprykerShop\Yves\ShopApplication\YvesBootstrap as SprykerYvesBootstrap;
+use SprykerShop\Yves\ShopApplication\Bootstrap\YvesBootstrap as SprykerYvesBootstrap;
 
 class YvesBootstrap extends SprykerYvesBootstrap
 {
-    /**
-     * @return void
-     */
-    protected function registerServiceProviders()
-    {
-        $this->application->register(new KernelLogServiceProvider());
-        $this->application->register(new CartServiceProvider());
-    }
 }
