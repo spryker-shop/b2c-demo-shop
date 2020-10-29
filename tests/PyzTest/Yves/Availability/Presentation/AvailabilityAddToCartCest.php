da734864422b680db52ddddb6e199e5401458664
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-10-29 10:15:21 +0200

    Update AvailabilityAddToCartCest.php

--- tests/PyzTest/Yves/Availability/Presentation/AvailabilityAddToCartCest.php
+++ tests/PyzTest/Yves/Availability/Presentation/AvailabilityAddToCartCest.php
@@ -34,6 +34,7 @@ class AvailabilityAddToCartCest
         $scenario->skip('This test needs to be refactored since we can not use add to cart URL directly.');
         
         $i->wantTo('Open product page, and add item to cart with larger quantity than available');
+        
         $i->expectTo('Display error message');
 
         $i->amLoggedInCustomer();
