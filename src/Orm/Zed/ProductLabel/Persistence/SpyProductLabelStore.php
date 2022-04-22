08650377fb597194d2ff0f95f18ad2c0ac53f17f
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2022-04-22 18:23:06 +0300

    Fixed merging issues

--- src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
+++ src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
@@ -5,21 +5,12 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-<<<<<<<< HEAD:src/Orm/Zed/OauthRevoke/Persistence/SpyOauthRefreshToken.php
-namespace Orm\Zed\OauthRevoke\Persistence;
-
-use Spryker\Zed\OauthRevoke\Persistence\Propel\AbstractSpyOauthRefreshToken as BaseSpyOauthRefreshToken;
-
-/**
- * Skeleton subclass for representing a row from the 'spy_oauth_refresh_token' table.
-========
 namespace Orm\Zed\ProductLabel\Persistence;
 
 use Spryker\Zed\ProductLabel\Persistence\Propel\AbstractSpyProductLabelStore as BaseSpyProductLabelStore;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_product_label_store' table.
->>>>>>>> internal/202204.0-release-testing:src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
  *
  *
  *
@@ -27,10 +18,6 @@ use Spryker\Zed\ProductLabel\Persistence\Propel\AbstractSpyProductLabelStore as 
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
-<<<<<<<< HEAD:src/Orm/Zed/OauthRevoke/Persistence/SpyOauthRefreshToken.php
-class SpyOauthRefreshToken extends BaseSpyOauthRefreshToken
-========
 class SpyProductLabelStore extends BaseSpyProductLabelStore
->>>>>>>> internal/202204.0-release-testing:src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
 {
 }
