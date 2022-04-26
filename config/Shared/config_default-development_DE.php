9ac85a38506bae9ffcff99bdb316f7699670ce40
Dima Aseev <dmitriy.aseev@spryker.com>
2022-04-26 16:31:39 +0300

    CC-16565: Adjusted configs.

--- config/Shared/config_default-development_DE.php
+++ config/Shared/config_default-development_DE.php
@@ -1,10 +1,5 @@
 <?php
 
-use ESpirit\Shared\FirstSpiritCaaS\FirstSpiritCaaSConstants;
 use Spryker\Shared\Propel\PropelConstants;
 
 $config[PropelConstants::ZED_DB_DATABASE] = 'DE_development_zed';
-
-// ----------- FirstSpirit CaaS Configurations
-$config[FirstSpiritCaaSConstants::FIRSTSPIRIT_CAAS_DATABASE_PREVIEW] = 'SprykerUpdateAT@preview';
-$config[FirstSpiritCaaSConstants::FIRSTSPIRIT_CAAS_DATABASE_ONLINE] = 'SprykerUpdateAT';
