2337350778d387c4d659bd198d04667d28b6f587
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2022-04-27 13:21:42 +0300

    Fixed merge issues

--- src/Orm/Zed/AssetStorage/Persistence/SpyAssetSlotStorage.php
+++ src/Orm/Zed/AssetStorage/Persistence/SpyAssetSlotStorage.php
@@ -5,21 +5,12 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-<<<<<<<< HEAD:src/Orm/Zed/Category/Persistence/SpyCategoryStore.php
-namespace Orm\Zed\Category\Persistence;
-
-use Spryker\Zed\Category\Persistence\Propel\AbstractSpyCategoryStore as BaseSpyCategoryStore;
-
-/**
- * Skeleton subclass for representing a row from the 'spy_category_store' table.
-========
 namespace Orm\Zed\AssetStorage\Persistence;
 
 use Spryker\Zed\AssetStorage\Persistence\Propel\AbstractSpyAssetSlotStorage as BaseSpyAssetSlotStorage;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_asset_slot_storage' table.
->>>>>>>> internal/master:src/Orm/Zed/AssetStorage/Persistence/SpyAssetSlotStorage.php
  *
  *
  *
@@ -27,10 +18,6 @@ use Spryker\Zed\AssetStorage\Persistence\Propel\AbstractSpyAssetSlotStorage as B
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
-<<<<<<<< HEAD:src/Orm/Zed/Category/Persistence/SpyCategoryStore.php
-class SpyCategoryStore extends BaseSpyCategoryStore
-========
 class SpyAssetSlotStorage extends BaseSpyAssetSlotStorage
->>>>>>>> internal/master:src/Orm/Zed/AssetStorage/Persistence/SpyAssetSlotStorage.php
 {
 }
