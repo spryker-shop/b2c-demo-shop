06c9d85a691a0049f8718a770158a38e93ae7115
Abid Hussain <abid.hussain@metacube.com>
2022-08-03 14:43:47 +0530

    CC-19107: Remove product fields in Cms block

--- src/Pyz/Zed/CmsBlockGui/CmsBlockGuiDependencyProvider.php
+++ src/Pyz/Zed/CmsBlockGui/CmsBlockGuiDependencyProvider.php
@@ -10,7 +10,6 @@ namespace Pyz\Zed\CmsBlockGui;
 use Spryker\Zed\CmsBlockCategoryConnector\Communication\Plugin\CmsBlockCategoryFormPlugin;
 use Spryker\Zed\CmsBlockCategoryConnector\Communication\Plugin\CmsBlockCategoryListViewPlugin;
 use Spryker\Zed\CmsBlockGui\CmsBlockGuiDependencyProvider as CmsBlockGuiCmsBlockGuiDependencyProvider;
-use Spryker\Zed\CmsBlockProductConnector\Communication\Plugin\CmsBlockProductAbstractFormPlugin;
 use Spryker\Zed\CmsBlockProductConnector\Communication\Plugin\CmsBlockProductAbstractListViewPlugin;
 use Spryker\Zed\ContentGui\Communication\Plugin\CmsBlockGui\HtmlToTwigExpressionsCmsBlockGlossaryBeforeSavePlugin;
 use Spryker\Zed\ContentGui\Communication\Plugin\CmsBlockGui\TwigExpressionsToHtmlCmsBlockGlossaryAfterFindPlugin;
@@ -19,28 +18,29 @@ use Spryker\Zed\Store\Communication\Plugin\Form\StoreRelationToggleFormTypePlugi
 class CmsBlockGuiDependencyProvider extends CmsBlockGuiCmsBlockGuiDependencyProvider
 {
     /**
-     * @return array
+     * @return array<string> $plugins
      */
     protected function getCmsBlockFormPlugins()
     {
         $plugins = parent::getCmsBlockFormPlugins();
         $plugins = array_merge($plugins, [
             new CmsBlockCategoryFormPlugin(),
-            new CmsBlockProductAbstractFormPlugin(),
         ]);
 
         return $plugins;
     }
 
     /**
-     * @return array
+     * @return array<string> $plugin
      */
     protected function getCmsBlockViewPlugins()
     {
-        return array_merge(parent::getCmsBlockViewPlugins(), [
+        $plugins = array_merge(parent::getCmsBlockViewPlugins(), [
             new CmsBlockCategoryListViewPlugin(),
             new CmsBlockProductAbstractListViewPlugin(),
         ]);
+
+        return $plugins;
     }
 
     /**
