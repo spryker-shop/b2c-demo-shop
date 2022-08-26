e8d679f0727b35cb2fb218ad81ba101938d85bec
Wojciech Pokój <wojciech.pokoj@accenture.com>
2022-08-26 09:25:42 +0200

    Work sync

---
+++ src/Pyz/Client/Faq/FaqsRestApiDependencyProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pyz\Client\FaqsRestApi;
+
+
+use Spryker\Client\Kernel\AbstractDependencyProvider;
+use Spryker\Client\Kernel\Container;
+
+class FaqsRestApiDependencyProvider extends AbstractDependencyProvider {
+
+    public const CLIENT_ZED_REQUEST = 'CLIENT_ZED_REQUEST';
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    public function provideServiceLayerDependencies(Container $container): Container
+    {
+        $container = $this->addZedRequestClient($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    protected function addZedRequestClient(Container $container): Container
+    {
+        $container->set(static::CLIENT_ZED_REQUEST,
+            function (Container $container) {
+                return $container->getLocator()->zedRequest()->client();
+            }
+        );
+
+        return $container;
+    }
+}
