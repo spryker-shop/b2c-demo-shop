5688c96ce9929acc29e2bbb5f0b6f78214dc6610
Oleksandr Kiiashko <80702205+oleksander-kiiashko@users.noreply.github.com>
2022-11-22 13:30:36 +0200

    APPS-5650: Integrate all ACP related changes into B2C Demo Shop (#1198)
    * [ACP] Local development added.
    
    * [BUGFIX] Added missing NPM versions.
    
    * [ACP] Code style fix.
    
    * [ACP] Removed ACP deploy file and DockerSDK versions changes.
    
    * [ACP] Code style fix.
    
    * [ACP] Revert gitkeep changes.
    
    * [ACP] Revert gitkeep changes.
    
    * APPS-5473: Integrate all modules required for ACP with internal B2C demo shop
    
    * APPS-5473: Integrate all modules required for ACP with internal B2C demo shop
    
    * APPS-5473: Integrate all modules required for ACP with internal B2C demo shop
    
    * [IMPROVEMENT] Moved out ACP local changes.
    
    * [IMPROVEMENT] Gitignore fix.
    
    * [ACP] Package name update.
    
    * APPS:5650 added integration
    
    * APPS:5650 composer update
    
    * APPS:5650 revert local ACP changes
    
    * APPS:5650 updated deploy.aop-demoshop-b2c.yml
    
    * APPS:5650 added deploy.aop-b2c-testing.yml
    
    * APPS:5650 reveret changes for deploy.aop-demoshop-b2c.yml
    
    * [APPS-5650] Trying to fix gzip issue.
    
    Co-authored-by: Anton Zubariev <anton.zubariev@spryker.com>
    Co-authored-by: Maksym Ohnyanikov <maksym.ohnianikov@spryker.com>
    Co-authored-by: Maksym Ohnianikov <89031985+Maksym-Ohnianikov@users.noreply.github.com>

--- config/Shared/config_default-ci.php
+++ config/Shared/config_default-ci.php
@@ -16,6 +16,8 @@ use Spryker\Shared\Kernel\KernelConstants;
 use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\Log\LogConstants;
 use Spryker\Shared\Mail\MailConstants;
+use Spryker\Shared\MessageBroker\MessageBrokerConstants;
+use Spryker\Shared\MessageBrokerAws\MessageBrokerAwsConstants;
 use Spryker\Shared\Newsletter\NewsletterConstants;
 use Spryker\Shared\OauthClient\OauthClientConstants;
 use Spryker\Shared\Product\ProductConstants;
@@ -271,3 +273,13 @@ $config[AppCatalogGuiConstants::OAUTH_PROVIDER_NAME] = OauthDummyConfig::PROVIDE
 //----------------------------------- ACP -------------------------------------
 //-----------------------------------------------------------------------------
 $config[ProductConstants::PUBLISHING_TO_MESSAGE_BROKER_ENABLED] = false;
+
+// ----------------------------------------------------------------------------
+// ------------------------------ MessageBroker -----------------------------------------
+// ----------------------------------------------------------------------------
+$config[MessageBrokerConstants::CHANNEL_TO_TRANSPORT_MAP] =
+$config[MessageBrokerAwsConstants::CHANNEL_TO_RECEIVER_TRANSPORT_MAP] = [
+    'payment' => 'in-memory',
+    'assets' => 'in-memory',
+    'product' => 'in-memory',
+];
