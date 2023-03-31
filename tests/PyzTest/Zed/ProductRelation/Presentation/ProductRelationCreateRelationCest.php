9f21856c34dcf60eb488e03dce600f109ae84cfb
Artem <artem.siminenko@gmail.com>
2023-03-31 13:57:23 +0200

    CC-23529 fix ProductRelationCreateRelationCest test

--- tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
+++ tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
@@ -8,6 +8,7 @@
 namespace PyzTest\Zed\ProductRelation\Presentation;
 
 use PyzTest\Zed\ProductRelation\PageObject\ProductRelationCreatePage;
+use PyzTest\Zed\ProductRelation\PageObject\ProductRelationTablePage;
 use PyzTest\Zed\ProductRelation\ProductRelationPresentationTester;
 use Spryker\Shared\ProductRelation\ProductRelationTypes;
 
@@ -24,22 +25,30 @@ use Spryker\Shared\ProductRelation\ProductRelationTypes;
 class ProductRelationCreateRelationCest
 {
     /**
+     * @var string
+     */
+    protected const PRODUCT_ABSTRACT_SKU = '214';
+
+    /**
      * @param \PyzTest\Zed\ProductRelation\ProductRelationPresentationTester $i
      *
      * @return void
      */
     public function testICanCreateProductRelationAndSeeInYves(ProductRelationPresentationTester $i): void
     {
+        $productRelationKey = uniqid('key-');
+        $i->cleanupProductRelationEntities(static::PRODUCT_ABSTRACT_SKU);
+
         $i->wantTo('I want to create up selling relation');
         $i->expect('relation is persisted, exported to yves and carousel component is visible');
 
         $i->amOnPage(ProductRelationCreatePage::URL);
 
         $i->waitForElement('//*[@id="product_relation_productRelationKey"]');
-        $i->fillField('//*[@id="product_relation_productRelationKey"]', uniqid('key-', false));
+        $i->fillField('//*[@id="product_relation_productRelationKey"]', $productRelationKey);
         $i->selectRelationType(ProductRelationTypes::TYPE_RELATED_PRODUCTS);
         $i->filterProductsByName('Samsung Bundle');
-        $i->selectProduct(214);
+        $i->selectProduct(static::PRODUCT_ABSTRACT_SKU);
 
         $i->switchToAssignProductsTab();
 
@@ -47,6 +56,6 @@ class ProductRelationCreateRelationCest
 
         $i->clickSaveButton();
 
-        $i->see(ProductRelationCreatePage::PRODUCT_SUCCESS_FULLY_CREATED_MESSAGE);
+        $i->checkProductRelationWasCreated($productRelationKey);
     }
 }
---
+++ tests/PyzTest/Zed/ProductRelation/_support/PageObject/ProductRelationTablePage.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PyzTest\Zed\ProductRelation\PageObject;
+
+class ProductRelationTablePage
+{
+    /**
+     * @var string
+     */
+    public const URL = '/product-relation-gui/list';
+}
