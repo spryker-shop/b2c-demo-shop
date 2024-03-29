f719b61632b02c2518c128d46e77ad49577858ed
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2024-03-29 14:19:14 +0200

    ACP-2521: fixed CI

---
+++ src/Orm/Zed/SalesPaymentDetail/Persistence/SpySalesPaymentDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\SalesPaymentDetail\Persistence;
+
+use Spryker\Zed\SalesPaymentDetail\Persistence\Propel\AbstractSpySalesPaymentDetail as BaseSpySalesPaymentDetail;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_sales_payment_detail' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpySalesPaymentDetail extends BaseSpySalesPaymentDetail
+{
+
+}
---
+++ src/Orm/Zed/SalesPaymentDetail/Persistence/SpySalesPaymentDetailQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\SalesPaymentDetail\Persistence;
+
+use Spryker\Zed\SalesPaymentDetail\Persistence\Propel\AbstractSpySalesPaymentDetailQuery as BaseSpySalesPaymentDetailQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_sales_payment_detail' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpySalesPaymentDetailQuery extends BaseSpySalesPaymentDetailQuery
+{
+
+}
