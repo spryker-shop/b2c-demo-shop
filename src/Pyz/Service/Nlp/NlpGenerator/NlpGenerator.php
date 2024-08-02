37a61afadd42ed3a924196762ded16fbdfb8dd0e
Vinod Agarwal <vinod.agarwal@nagarro.com>
2024-08-02 12:24:28 +0530

    Fix prompt in nlp generator

--- src/Pyz/Service/Nlp/NlpGenerator/NlpGenerator.php
+++ src/Pyz/Service/Nlp/NlpGenerator/NlpGenerator.php
@@ -47,7 +47,7 @@ class NlpGenerator implements NlpGeneratorInterface
                     "role" => "user",
                     "parts" => [
                         [
-                            "text" => $text.' from this phrase extract the product information in below format {
+                            "text" => '"'.$text.'" from this phrase extract the product information in below format {
                             "product_type": "extracted_value",
                             "attributes": [
                             {
@@ -62,7 +62,7 @@ class NlpGenerator implements NlpGeneratorInterface
                             }
                             ]
                             }
-                            , min max are the values if attribute value is numeric. if no min or max intention is found then key can be omitted from result . value should contain only rounded numeric value , not the below , above  intents. attribute key can be one of from the list : ",torage_capacity,series,width,white_balance,weight,waterproof_up_to,waterproof,voice_recording,video_recording_modes,usb_version,usb_port,touchscreen,touch_technology,total_megapixels,thermal_design_power,themes,tcase,system_bus_rate,storage_media,stepping,sim_card_type,shape,sensor_type,self_timer,scenario_design_power,recording_time,rear_camera_resolution,rear_camera,protection_feature,product_type,processor_threads,processor_socket,processor_operating_models,processor_model,processor_frequency,processor_cores,processor_codename,processor_cache,processor_cache_type,processor_boost_frequency,photo_effects,pci_express_slots_version,pci_express_configurations,os_version,os_installed,optical_zoom,optical_sensor_size,mhl_version,memory_slots,megapixel,max_memory_card_size,internal_memory,light_exposure_modes,iso_sensitivity,internal_storage_capacity,internal_ram,intel_smart_cache,image_processor,headphone_connectivity,hdmi,hd_type,graphics_adapter,gps_satellite,full_hd,fsb_parity,front_camera_resolution,form_factor,focus_adjustment,focus,flash_type,flash_range_tele,flash_memory,fingerprint_reader,filter_size,field_of_view,face_detection,effective_megapixels,display_type,display_technology,display_diagonal,display,digital_zoom,cpu_configuration,control_type,compatible_memory_cards,combined_zoom,color,clock_mode,chassis_type,camera_type,camcorder_type,camcorder_media_type,bus_type,brand,bluetooth_version,battery_type,battery_life,backlight,auto_focus,audio_system,aspect_ratio,alarm_clock,4g_standards,4g,os_system,wi_fi,total_storage_capacity,bundled_product,packaging_unit,upcs
+                            , min max are the values if attribute value is numeric. if no min or max intention is found then key can be omitted from result . value should contain only rounded numeric value , not the below , above  intents. attribute key can be one of from the list : ",torage_capacity,series,width,white_balance,weight,waterproof_up_to,waterproof,voice_recording,video_recording_modes,usb_version,usb_port,touchscreen,touch_technology,total_megapixels,thermal_design_power,themes,tcase,system_bus_rate,storage_media,stepping,sim_card_type,shape,sensor_type,self_timer,scenario_design_power,recording_time,rear_camera_resolution,rear_camera,protection_feature,product_type,processor_threads,processor_socket,processor_operating_models,processor_model,processor_frequency,processor_cores,processor_codename,processor_cache,processor_cache_type,processor_boost_frequency,photo_effects,pci_express_slots_version,pci_express_configurations,os_version,os_installed,optical_zoom,optical_sensor_size,mhl_version,memory_slots,megapixel,max_memory_card_size,internal_memory,light_exposure_modes,iso_sensitivity,internal_storage_capacity,internal_ram,intel_smart_cache,image_processor,headphone_connectivity,hdmi,hd_type,graphics_adapter,gps_satellite,full_hd,fsb_parity,front_camera_resolution,form_factor,focus_adjustment,focus,flash_type,flash_range_tele,flash_memory,fingerprint_reader,filter_size,field_of_view,face_detection,effective_megapixels,display_type,display_technology,display_diagonal,display,digital_zoom,cpu_configuration,control_type,compatible_memory_cards,combined_zoom,color,clock_mode,chassis_type,camera_type,camcorder_type,camcorder_media_type,bus_type,brand,bluetooth_version,battery_type,battery_life,backlight,auto_focus,audio_system,aspect_ratio,alarm_clock,4g_standards,4g,os_system,wi_fi,total_storage_capacity,bundled_product,packaging_unit,upcs,carbon_emission
                             "'
                         ],
                     ]
@@ -83,7 +83,7 @@ class NlpGenerator implements NlpGeneratorInterface
         $apiEndpoint = "us-central1-aiplatform.googleapis.com";
         $modelId = "gemini-1.5-flash-001";
 
-     
+
         $curl = curl_init();
 
         curl_setopt_array($curl, [
