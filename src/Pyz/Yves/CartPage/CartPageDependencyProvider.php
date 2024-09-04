f17907484c068bd8428409335366b92e60f68ef3
Dmytro Asieiev <dmiseev@gmail.com>
2024-09-04 13:05:11 +0300

    CC-13685: Updated product quantity and total in Mini Cart Widget from catalog AJAX add-to-cart action.

--- src/Pyz/Yves/CartPage/CartPageDependencyProvider.php
+++ src/Pyz/Yves/CartPage/CartPageDependencyProvider.php
@@ -7,55 +7,14 @@
 
 namespace Pyz\Yves\CartPage;
 
-use Pyz\Yves\CartPage\Plugin\Cart\CartBlockMiniCartViewExpanderPlugin;
-use Spryker\Yves\Kernel\Container;
 use SprykerShop\Yves\CartPage\CartPageDependencyProvider as SprykerCartPageDependencyProvider;
+use SprykerShop\Yves\CartPage\Plugin\CartPage\CartBlockMiniCartViewExpanderPlugin;
 use SprykerShop\Yves\DiscountPromotionWidget\Plugin\CartPage\DiscountPromotionAddToCartFormWidgetParameterExpanderPlugin;
 use SprykerShop\Yves\ProductBundleWidget\Plugin\CartPage\ProductBundleCartItemTransformerPlugin;
 use SprykerShop\Yves\UrlPage\Plugin\CartPage\UrlCartItemTransformerPlugin;
 
 class CartPageDependencyProvider extends SprykerCartPageDependencyProvider
 {
-    /**
-     * @var string
-     */
-    public const TWIG_ENVIRONMENT = 'TWIG_ENVIRONMENT';
-
-    /**
-     * @uses \Spryker\Yves\Twig\Plugin\Application\TwigApplicationPlugin::SERVICE_TWIG
-     *
-     * @var string
-     */
-    protected const SERVICE_TWIG = 'twig';
-
-    /**
-     * @param \Spryker\Yves\Kernel\Container $container
-     *
-     * @return \Spryker\Yves\Kernel\Container
-     */
-    public function provideDependencies(Container $container): Container
-    {
-        $container = parent::provideDependencies($container);
-
-        $container = $this->addTwigService($container);
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Yves\Kernel\Container $container
-     *
-     * @return \Spryker\Yves\Kernel\Container
-     */
-    protected function addTwigService(Container $container): Container
-    {
-        $container->set(static::TWIG_ENVIRONMENT, function (Container $container) {
-            return $container->getApplicationService(static::SERVICE_TWIG);
-        });
-
-        return $container;
-    }
-
     /**
      * @return array<\SprykerShop\Yves\CartPageExtension\Dependency\Plugin\CartItemTransformerPluginInterface>
      */
