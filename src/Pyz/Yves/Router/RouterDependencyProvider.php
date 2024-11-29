baa8d5c57356401f438e97caadfcce9801298f63
dimitriyTsemma <dmitry.tsemma@spryker.com>
2024-11-29 14:56:39 +0200

    FRW-7936 Integration.

--- src/Pyz/Yves/Router/RouterDependencyProvider.php
+++ src/Pyz/Yves/Router/RouterDependencyProvider.php
@@ -147,8 +147,8 @@ class RouterDependencyProvider extends SprykerRouterDependencyProvider
     protected function getRouterEnhancerPlugins(): array
     {
         return [
-            new LanguagePrefixRouterEnhancerPlugin(),
             new StorePrefixRouterEnhancerPlugin(),
+            new LanguagePrefixRouterEnhancerPlugin(),
         ];
     }
 }
