e6f50f2cd077c72e5330115297666562dd3b984e
ordukaya-uemit <ordukaya.uemit@gmail.com>
2025-01-15 16:29:07 +0100

    Logik implementiert

--- src/Pyz/Zed/RomanNumbers/Business/Converter/RomanNumbersConverter.php
+++ src/Pyz/Zed/RomanNumbers/Business/Converter/RomanNumbersConverter.php
@@ -4,10 +4,34 @@ namespace Pyz\Zed\RomanNumbers\Business\Converter;
 
 class RomanNumbersConverter
 {
+    const array numbers = [
+"I" => 1,
+"V" => 5,
+"X" => 10,
+"L" => 50,
+"C" => 100,
+"D" => 500,
+"M" => 1000,
+];
     public function convert(string $romanNumbers): int
     {
-        $result = 1;
-        // TODO: add logic to convert roman numbers to integer
+
+        $romanNumbers = str_replace("IV","IIII",$romanNumbers);
+        $romanNumbers = str_replace("IX","VIIII",$romanNumbers);
+        $romanNumbers = str_replace("XL","XXXX",$romanNumbers);
+        $romanNumbers = str_replace("XC","LXXXX",$romanNumbers);
+        $romanNumbers = str_replace("CD","CCCC",$romanNumbers);
+        $romanNumbers = str_replace("CM","DCCCC",$romanNumbers);
+        $romanNumbers = str_replace("XD","CCCCLXXXX",$romanNumbers);
+
+        $result = 0;
+        $result += substr_count($romanNumbers, 'I');
+        $result += substr_count($romanNumbers, 'V')*5;
+        $result += substr_count($romanNumbers, 'X')*10;
+        $result += substr_count($romanNumbers, 'L')*50;
+        $result += substr_count($romanNumbers, 'C')*100;
+        $result += substr_count($romanNumbers, 'D')*500;
+        $result += substr_count($romanNumbers, 'M')*1000;
         return $result;
     }
 }
--- tests/PyzTest/Zed/RomanNumbers/Business/RomanNumbersFacadeTest.php
+++ tests/PyzTest/Zed/RomanNumbers/Business/RomanNumbersFacadeTest.php
@@ -21,6 +21,19 @@ class RomanNumbersFacadeTest extends Unit
      */
     protected $tester;
 
+    public function testConversionOfRomanNumberToIntegerSpaces(): void
+    {
+        // Arrange
+        $romanNumber = ' M C III  ';
+        $expectedInteger = 1103;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
     public function testConversionOfRomanNumberToIntegerOne(): void
     {
         // Arrange
@@ -39,6 +52,227 @@ class RomanNumbersFacadeTest extends Unit
         // Arrange
         $romanNumber = 'II';
         $expectedInteger = 2;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
+    public function testConversionOfRomanNumberToIntegerThree(): void
+    {
+        // Arrange
+        $romanNumber = 'III';
+        $expectedInteger = 3;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
+    public function testConversionOfRomanNumberToIntegerFive(): void
+    {
+        // Arrange
+        $romanNumber = 'V';
+        $expectedInteger = 5;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
+    public function testConversionOfRomanNumberToIntegerSix(): void
+    {
+        // Arrange
+        $romanNumber = 'VI';
+        $expectedInteger = 6;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
+    public function testConversionOfRomanNumberToIntegerFour(): void
+    {
+        // Arrange
+        $romanNumber = 'IV';
+        $expectedInteger = 4;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+
+    public function testConversionOfRomanNumberToIntegerTen(): void
+    {
+        // Arrange
+        $romanNumber = 'X';
+        $expectedInteger = 10;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerNine(): void
+{
+    // Arrange
+    $romanNumber = 'IX';
+    $expectedInteger = 9;
+
+    // Act
+    $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+    // Assert
+    $this->assertSame($expectedInteger, $result);
+}
+    public function testConversionOfRomanNumberToIntegerFifty(): void
+    {
+        // Arrange
+        $romanNumber = 'L';
+        $expectedInteger = 50;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerFourthy(): void
+    {
+        // Arrange
+        $romanNumber = 'XL';
+        $expectedInteger = 40;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerHundred(): void
+    {
+        // Arrange
+        $romanNumber = 'C';
+        $expectedInteger = 100;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerNinty(): void
+    {
+        // Arrange
+        $romanNumber = 'XC';
+        $expectedInteger = 90;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerFivehundred(): void
+    {
+        // Arrange
+        $romanNumber = 'D';
+        $expectedInteger = 500;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerFourhundredNinty(): void
+    {
+        // Arrange
+        $romanNumber = 'XD';
+        $expectedInteger = 490;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerTausend(): void
+    {
+        // Arrange
+        $romanNumber = 'M';
+        $expectedInteger = 1000;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerNinehundredNinty(): void
+    {
+        // Arrange
+        $romanNumber = 'CMXC';
+        $expectedInteger = 990;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerThreetausendNintynine(): void
+    {
+        // Arrange
+        $romanNumber = 'MMMCMXCIX';
+        $expectedInteger = 3999;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerTwotausendfourhundredfourthy(): void
+    {
+        // Arrange
+        $romanNumber = 'MMCDXL';
+        $expectedInteger = 2440;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerFourhundred(): void
+    {
+        // Arrange
+        $romanNumber = 'CD';
+        $expectedInteger = 400;
+
+        // Act
+        $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
+
+        // Assert
+        $this->assertSame($expectedInteger, $result);
+    }
+    public function testConversionOfRomanNumberToIntegerNinehundred(): void
+    {
+        // Arrange
+        $romanNumber = 'CM';
+        $expectedInteger = 900;
 
         // Act
         $result = $this->tester->getFacade()->convertRomanToInteger($romanNumber);
