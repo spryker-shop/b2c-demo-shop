1893636dd00258a52fc306b7d3fc863ff5f37b24
dmiseev <dmitriy.aseev@spryker.com>
2025-06-27 15:01:31 +0200

    Added sub-steps for e2e flow.

--- src/Pyz/Zed/AuthorizationPickingAppBackendApi/AuthorizationPickingAppBackendApiConfig.php
+++ src/Pyz/Zed/AuthorizationPickingAppBackendApi/AuthorizationPickingAppBackendApiConfig.php
@@ -1,9 +1,11 @@
 <?php
 
 /**
- * This file is part of the Spryker Suite.
+ * This file is part of the Spryker Commerce OS.
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
+
+declare(strict_types = 1);
 
 namespace Pyz\Zed\AuthorizationPickingAppBackendApi;
 
